eda447a79b25d7d2f84d4f53390986f5fedb048f
The-6th-Champion <The6thChampion1883@protonmail.com>
2022-04-01 17:01:53 -0700

    FIRST BUTTON WORKING!!!

--- Mobile/HapticMetroBand/App.tsx
+++ Mobile/HapticMetroBand/App.tsx
@@ -1,27 +1,35 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {Text, Button, View, StyleSheet, Pressable} from 'react-native';
+import React, { ReactElement } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet, Pressable } from "react-native";
+
+const Button: any = (props: any) => {
+  console.log(props.text);
+  return (
+    <Pressable
+      style={({ pressed }) => {
+        return [{ backgroundColor: pressed ? "blue" : "red" }, styles.button];
+      }}
+      onPress={props.onPress}
+    >
+      <Text>{props.text}</Text>
+    </Pressable>
+  );
+};
 
 export default function App(): JSX.Element {
-
   const on = (): void => {
-
-  }
+    console.log("on");
+  };
 
   const off = (): void => {
-    
-  }
+    console.log("off");
+  };
 
   return (
     <View style={styles.container}>
+      <Button text="On" style={[styles.button]} onPress={on}></Button>
 
-      <Pressable style={styles.button} onPress={on}>
-        <Text>On</Text>
-      </Pressable>
-
-      <Pressable style={styles.button} onPress={off}>
-        <Text>Off</Text>
-      </Pressable>
+      <Button text="Off" style={styles.button} onPress={off}></Button>
 
       <StatusBar style="auto" />
     </View>
@@ -31,17 +39,16 @@ export default function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2E3440',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }, 
+    backgroundColor: "#2E3440",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   button: {
-    backgroundColor: '#A2A3BB',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 10,
-    paddingHorizontal: 30,
-    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 30,
+    paddingHorizontal: 80,
+    borderRadius: 15,
     elevation: 3,
-  }
+  },
 });
