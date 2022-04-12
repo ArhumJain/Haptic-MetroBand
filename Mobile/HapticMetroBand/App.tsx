82cec5df3184eb18316e58847895d22433bf4a73
Sid Shah <The6thChampion1883@protonmail.com>
2022-04-11 21:53:35 -0700

    scrolling for screen overflow

--- Mobile/HapticMetroBand/App.tsx
+++ Mobile/HapticMetroBand/App.tsx
@@ -1,6 +1,13 @@
 import React, { ReactElement } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View, StyleSheet, Pressable, SafeAreaView } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 import Screen1 from "./components/Screen1";
 import styles from "./Styles";
 
@@ -8,7 +15,9 @@ export default function App(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Screen1 style={styles.screen} />
+      <ScrollView>
+        <Screen1 style={styles.screen} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
