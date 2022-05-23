a0948585bfd3eadf32548a1481ee35d46ba90a30
Arhum Jain <arhumjain10@gmail.com>
2022-05-22 19:37:32 -0700

    graph improvements

--- Data/analysis.py
+++ Data/analysis.py
@@ -2,12 +2,21 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import numpy as np
 
-bpm60 = np.loadtxt("./60met.txt", delimiter="\n")
-bpm120 = np.loadtxt("./120met.txt", delimiter="\n")
-bpm240 = np.loadtxt("./240met.txt", delimiter="\n")
+bpm60Metroband = np.loadtxt("./60bpm.txt", delimiter="\n")
+bpm120Metroband = np.loadtxt("./120bpm.txt", delimiter="\n")
+bpm240Metroband = np.loadtxt("./240bpm.txt", delimiter="\n")
 
-bpmData = [bpm60, bpm120, bpm240]
-graph = 0
+bpm60Computer = np.loadtxt("./60met.txt", delimiter="\n")
+bpm120Computer = np.loadtxt("./120met.txt", delimiter="\n")
+bpm240Computer = np.loadtxt("./240met.txt", delimiter="\n")
+
+bpmData = [(bpm60Metroband, "Interval lengths for 60 BPM (Haptic Metroband)"), 
+           (bpm120Metroband, "Interval lengths for 120 BPM (Haptic Metroband)"), 
+           (bpm240Metroband, "Interval lengths for 240 BPM (Haptic Metroband)"), 
+           (bpm60Computer, "Interval lengths for 60 BPM (Computer)"), 
+           (bpm120Computer, "Interval lengths for 120 BPM (Computer)"), 
+           (bpm240Computer, "Interval lengths for 240 BPM (Computer)")]
+graph = 5
 
 plt.style.use('ggplot')
 
@@ -15,7 +24,7 @@ fig, ax = plt.subplots()
 fig.set_figheight(10)
 fig.set_figwidth(20)
 
-plot = ax.boxplot(bpmData[graph], patch_artist=True,
+plot = ax.boxplot(bpmData[graph][0], patch_artist=True,
                 showmeans=False, showfliers=True, widths=(0.1),
                 medianprops={"color": "white", "linewidth": 0.5},
                 boxprops={"facecolor": "C0", "edgecolor": "white",
@@ -23,10 +32,10 @@ plot = ax.boxplot(bpmData[graph], patch_artist=True,
                 whiskerprops={"color": "C0", "linewidth": 1.5},
                 capprops={"color": "C0", "linewidth": 1.5}, vert=False)
 
-ax.set_title(f"Interval lengths for 60 BPM")
+ax.set_title(bpmData[graph][1])
 
 plt.table(
-    cellText=[[round(np.mean(bpmData[graph]), 3), round(np.std(bpmData[graph]), 3), np.median(bpmData[graph]), np.size(bpmData[graph])]],
+    cellText=[[round(np.mean(bpmData[graph][0]), 3), round(np.std(bpmData[graph][0]), 3), np.median(bpmData[graph][0]), np.size(bpmData[graph][0])]],
     cellLoc='center',
     colLabels=["Mean", "Standard Deviation", "Median", "N"],
     colColours=[(0.749,0.749,0.749,1)]*8,
@@ -34,7 +43,7 @@ plt.table(
     bbox=[0, -0.3, 0.3, 0.1]
 )
 
-ax.set(xticks=np.arange(min(bpmData[graph]), max(bpmData[graph]), 10))
+ax.set(xticks=np.arange(min(bpmData[graph][0]), max(bpmData[graph][0]), 10))
 
 plt.subplots_adjust(bottom=0.4)
 plt.show()
